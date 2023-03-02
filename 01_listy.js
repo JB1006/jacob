eda781969e5a92ab9caec35231eab1c308b45993
Jakub Bartnik <kubabuba446@wp.pl>
2023-03-02 14:11:25 +0100

    cc

---
+++ 01_listy.js
@@ -0,0 +1,10 @@
+let firstList = [1, 3.5, 7, "jeden", "dwa", [1,2,3]];
+let SecondList = [[1, 2, 5], [7, 5, 1]];
+
+console.log (firstList)
+console.log (SecondList)
+
+console.log(firstList[0])
+console.log(SecondList[0])
+console.log(firstList[5])
+console.log(SecondList[1])
