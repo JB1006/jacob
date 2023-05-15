60f8a346319e3c2832c8da749613f0bb410ae711
Jakub Bartnik <kubabuba446@wp.pl>
2023-05-15 13:14:58 +0200

    Dom

---
+++ Dom/01_dom.js
@@ -0,0 +1,36 @@
+let h1Element=document.getElementById(`h1`)
+// innerHTML
+
+function ChangeText(){
+    h1Element.innerHTML=`jestem kliknięty`
+}
+
+h1Element.addEventListener(`click`, ChangeText)
+
+const blueBtnElement=document.getElementById(`blueBtn`)
+console.log(blueBtnElement)
+
+// zad.2
+function ChangeBackgroundColor(){
+    
+        document.body.style.backgroundColor="lightblue"
+}
+
+blueBtnElement.addEventListener(`click`, ChangeBackgroundColor)
+
+//zad.3
+const randomBtn=document.getElementById(`buttonRandom`)
+const randomNumber=document.getElementById(`randomNumber`)
+//Math random floor
+function generateRandom(max){
+    return Math.floor(Math.random()*max)
+    
+}
+function randomNumberGenerator(){
+    randomNumber.innerHTML=generateRandom(100)
+}
+
+randomBtn.addEventListener(`click`,randomNumberGenerator)
+
+
+
---
+++ Jakub Bartnik funkcje/funkcje.js
@@ -0,0 +1,8 @@
+console.log("Jakub Bartnik zad.2")
+function tellFortune(NumberChildren, partnerName, Location, Job){
+    console.log(`You will be a ${partnerName} in ${location}, and married to Z with ${NumberChildren} kids.`)
+}
+
+tellFortune(2, "Jakub", "Londyn", "Kucharz")
+tellFortune(3, "Marian", "Madryt", "Policjant")
+tellFortune(5, "Wojtek", "Warszawa", "Malarz")
